f85b6b6d30116f2346814ca6c41609c0d8c10608
Mark A. Grondona <mark.grondona@gmail.com>
2024-12-08 00:26:31 +0000

    pam_flux: support allow-guest-user option
    Problem: The pam_flux module only allows users onto a node if
    they currently have an active job assigned to that node. This
    simplistic access control is not sufficient if guest users need
    to access a multi-user instance of Flux running as a job in the
    system instance. Any attempt to use flux-proxy to interact with the
    subinstance will fail, because the ssh connector will be denied access
    by pam_flux.so.
    
    Add a new config parameter `allow-guest-user` to the flux_pam module.
    If this option is set, the module will add the current instance
    owner to the job-list query it uses to determine access. Access will
    additionally be granted if there is job for the instance owner and
    all of the following are true:
    
     - the current node is rank 0 of the job
     - the job is an instance of Flux
     - the instance is configured with access.allow-guest-user
    
    Of course, access is also granted if the user has an active job on
    the current node.
    
    Fixes #7

--- src/pam/pam_flux.c
+++ src/pam/pam_flux.c
@@ -32,9 +32,20 @@
 #include <jansson.h>
 
 #include <flux/core.h>
+#include <flux/idset.h>
 
 #define PAM_SM_ACCOUNT
 #include <security/pam_modules.h>
+
+struct options {
+    /*  If set, permit access to all users if the specified user has
+     *  a job in RUN state on this host, this is rank 0 of that job,
+     *  the job is an instance of Flux, and has access.allow-guest-user
+     *  configured.
+     *  (Allows guests to access multi-user instance jobs via ssh connector)
+     */
+    bool allow_guest_user;
+};
 
 /*
  *  Write message described by the 'format' string to syslog.
@@ -51,10 +62,139 @@ static void log_msg (int level, const char *format, ...)
     return;
 }
 
-/*  Return 1 if uid has the local rank currently allocated to an active
- *   Flux job.
+static char *uri_to_local (const char *uri)
+{
+    char *local_uri = NULL;
+    char *p;
+
+    /* Ensure this uri starts with `ssh://`
+     */
+    if (!uri || strncmp (uri, "ssh://", 6) != 0)
+        return NULL;
+
+    /* Skip to next '/' after ssh:// part
+     */
+    if (!(p = strchr (uri+6, '/')))
+        return NULL;
+
+    /* Construct local uri from remainder (path)
+     */
+    if (asprintf (&local_uri, "local:///%s", p) < 0)
+        return NULL;
+    return local_uri;
+}
+
+/* Return 1 if local instance at uri has access.allow-guest-user=true.
+ * Return 0 otherwise.
  */
-static int flux_check_user (uid_t uid)
+static int check_guest_allowed (const char *uri)
+{
+    int allowed = 0;
+    flux_t *h = NULL;
+    flux_future_t *f = NULL;
+    char *local_uri = NULL;
+
+    if (!uri)
+        goto out;
+
+    if (!(local_uri = uri_to_local (uri))) {
+        log_msg (LOG_ERR, "failed to transform %s into local uri", uri);
+        goto out;
+    }
+    if (!(h = flux_open (local_uri, 0))) {
+        log_msg (LOG_ERR, "flux_open (%s): %m", local_uri);
+        goto out;
+    }
+    if (!(f = flux_rpc (h, "config.get", NULL, FLUX_NODEID_ANY, 0))
+        || flux_rpc_get_unpack (f,
+                                "{s?{s?b}}",
+                                "access",
+                                 "allow-guest-user", &allowed) < 0) {
+        log_msg (LOG_ERR, "failed to get config: %m");
+        goto out;
+    }
+    if (!allowed)
+        log_msg (LOG_INFO, "access.allow-guest-user not enabled in child");
+out:
+    flux_close (h);
+    free (local_uri);
+    return allowed;
+}
+
+/* Loop over jobs in json array 'jobs'.
+ * - If any job owner is uid, permit.
+ * - If any job owner is allow_if_user and rank == rank 0 of the job,
+ *   permit if the job is an instance (has a uri) and acesss.allow-guest-user
+ *   is true.
+ */
+static int check_jobs_array (json_t *jobs,
+                             unsigned int rank,
+                             uid_t uid,
+                             uid_t allow_if_user)
+{
+    size_t index;
+    json_t *entry;
+
+    json_array_foreach (jobs, index, entry) {
+        const char *job_ranks;
+        const char *uri = NULL;
+        int job_uid;
+
+        if (json_unpack (entry,
+                         "{s:i s:s s?{s?{s?s}}}",
+                         "userid", &job_uid,
+                         "ranks", &job_ranks,
+                         "annotations",
+                          "user",
+                           "uri", &uri) < 0) {
+            log_msg (LOG_ERR, "failed to unpack userid, ranks for job");
+            return 0;
+        }
+        if (job_uid == uid)
+            return 1;
+        else if (job_uid == allow_if_user) {
+            struct idset *ranks;
+            if ((ranks = idset_decode (job_ranks))) {
+                int allowed = 0;
+                /* Only if this rank is rank 0 of the job, check that
+                 * access.allow-guest-user is enabled in the job instance:
+                 */
+                if (rank == idset_first (ranks))
+                    allowed = check_guest_allowed (uri);
+                idset_destroy (ranks);
+                if (allowed)
+                    return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+/* Fetch an attribute and return its value as uid_t.
+ */
+static uid_t attr_get_uid (flux_t *h, const char *name)
+{
+    const char *s;
+    char *endptr;
+    long i;
+
+    if (!(s = flux_attr_get (h, name))) {
+        log_msg (LOG_ERR, "flux_attr_get (%s): %m", name);
+        return (uid_t) -1;
+    }
+    errno = 0;
+    i = strtol (s, &endptr, 10);
+    if (errno != 0 || *endptr != '\0') {
+        log_msg (LOG_ERR, "error converting %s to uid: %m", name);
+        return (uid_t) -1;
+    }
+    return (uid_t) i;
+}
+
+
+/*  get jobs in RUN state on this node for user(s) of interest:
+ */
+static int flux_check_user (struct options *opts, uid_t uid)
 {
     int authorized = 0;
     json_t *jobs = NULL;
@@ -63,6 +203,13 @@ static int flux_check_user (uid_t uid)
     char rankstr[16];
     flux_future_t *f = NULL;
 
+    /* allow_if_user MAY be set to the instance owner to allow guest
+     * access for uid to this node in the case of a multi-user subinstance.
+     * However, initialize it to uid so it can unconditionally be used below
+     * in the RPC to job-list, which greatly simplifies code.
+     */
+    uid_t allow_if_user = uid;
+
     if (!(h = flux_open (NULL, 0))) {
         log_msg (LOG_ERR, "Unable to connect to Flux: %m");
         return 0;
@@ -70,6 +217,14 @@ static int flux_check_user (uid_t uid)
     if (flux_get_rank (h, &rank) < 0) {
         log_msg (LOG_ERR, "Failed to get current broker rank: %m");
         goto out;
+    }
+    if (opts->allow_guest_user) {
+        uid_t owner = attr_get_uid (h, "security.owner");
+        if (owner != (uid_t) -1)
+            allow_if_user = owner;
+        else
+            log_msg (LOG_ERR,
+                     "Failed to get security.owner, can't allow guest access");
     }
     if (snprintf (rankstr,
                   sizeof (rankstr),
@@ -85,12 +240,12 @@ static int flux_check_user (uid_t uid)
                        "job-list.list",
                        0,
                        0,
-                       "{s:i s:[ss] s:{s:[{s:[i]} {s:[s]} {s:[i]}]}}",
+                       "{s:i s:[sss] s:{s:[{s:[ii]} {s:[s]} {s:[i]}]}}",
                        "max_entries", 0,
-                       "attrs", "ranks", "state",
+                       "attrs", "userid", "ranks", "annotations",
                        "constraint",
                         "and",
-                         "userid", uid,
+                         "userid", uid, allow_if_user,
                          "ranks", rankstr,
                          "states", FLUX_JOB_STATE_RUN);
     if (!f || flux_rpc_get_unpack (f, "{s:o}", "jobs", &jobs) < 0) {
@@ -98,10 +253,7 @@ static int flux_check_user (uid_t uid)
         goto out;
     }
 
-    /* If there was at least one job returned, then this user is allowed
-     */
-    if (json_array_size (jobs) > 0)
-        authorized = 1;
+    authorized = check_jobs_array (jobs, rank, uid, allow_if_user);
 
 out:
     flux_future_destroy (f);
@@ -167,6 +319,23 @@ static void send_denial_msg (pam_handle_t *pamh,
     return;
 }
 
+static int parse_options (struct options *opts, int argc, const char **argv)
+{
+    for (int i = 0; i < argc; i++) {
+        if (strcmp ("allow-guest-user", argv[i]) == 0) {
+            opts->allow_guest_user = true;
+        }
+        else {
+            log_msg (LOG_ERR,
+                    "unrecognized option: %s",
+                    argv[i]);
+            return -1;
+        }
+    }
+    return 0;
+}
+
+
 PAM_EXTERN int
 pam_sm_acct_mgmt (pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
@@ -175,6 +344,7 @@ pam_sm_acct_mgmt (pam_handle_t *pamh, int flags, int argc, const char **argv)
     struct passwd *pw;
     uid_t uid;
     int auth = PAM_PERM_DENIED;
+    struct options opts = { .allow_guest_user = false };
 
     retval = pam_get_item (pamh, PAM_USER, (const void **) &user);
     if ((retval != PAM_SUCCESS) || (user == NULL) || (*user == '\0')) {
@@ -189,7 +359,10 @@ pam_sm_acct_mgmt (pam_handle_t *pamh, int flags, int argc, const char **argv)
     }
     uid = pw->pw_uid;
 
-    if (flux_check_user (uid))
+    if (parse_options (&opts, argc, argv) < 0)
+        return PAM_SYSTEM_ERR;
+
+    if (flux_check_user (&opts, uid))
         auth = PAM_SUCCESS;
 
     if (auth != PAM_SUCCESS)
